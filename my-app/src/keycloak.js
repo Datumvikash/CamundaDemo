dc9681cf113c5776bed79f39cc61715b8d88c23c
Datumvikash <vikash.k.gupta@zohomail.in>
2025-11-16 15:51:30 +0530

    Update keycloak.js

--- my-app/src/keycloak.js
+++ my-app/src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://63.178.79.202:18080/auth', // Camunda Keycloak URL
+  url: 'http://XXXXXXXXX:18080/auth', // Camunda Keycloak URL
   realm: 'camunda-platform',
   clientId: 'react-app',
 });
