e2f5d92bb63b588a4013a09d03379f86566734ec
Datumvikash <vikash.k.gupta@zohomail.in>
2025-11-16 15:51:53 +0530

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: { '/api': { target: 'http://63.178.79.202:8090', changeOrigin: true } }
+    proxy: { '/api': { target: 'http://XXXXXXXXX:8090', changeOrigin: true } }
   }
 })
